601aac2ef7e979b200d886db16615ea84514b71e
MaxX <maxx-f@mail.ru>
2022-08-29 20:44:32 +0300

    Add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max) // Задать двумерный массив
+{
+    int[,] array = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            array[row, column] = new Random().Next(min, max+1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] arr) // Печать двумерного массива
+{
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int column = 0; column < arr.GetLength(1); column++)
+        {
+            Console.Write($"{arr[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Ordering(int[,] arr) // Замена строк на столбцы
+{
+    if (arr.GetLength(0) == arr.GetLength(1))
+    {
+        int[,] nums = new int[arr.GetLength(0), arr.GetLength(1)];
+        for (int m = 0; m < arr.GetLength(0); m++)
+        {
+            for (int n = 0; n < arr.GetLength(1); n++)
+            {
+                nums[m, n] = arr[m, n];
+            }
+        }
+        for (int m = 0; m < arr.GetLength(0); m++)
+        {
+            for (int n = 0; n < arr.GetLength(1); n++)
+            {
+                arr[m, n] = nums[n, m];
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Матрица не квадратная");
+    }
+}
+int[,] matrix = CreateMatrix(3, 3, 0, 99);
+PrintMatrix(matrix);
+
+Console.WriteLine();
+Ordering(matrix);
+PrintMatrix(matrix);
